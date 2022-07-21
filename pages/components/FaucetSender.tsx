4dfec824f840e1cc2de300c4b6187335565640aa
Yuki <59007319+Yukit02@users.noreply.github.com>
2022-07-21 19:12:54 +0900

    feat: send token

--- pages/components/FaucetSender.tsx
+++ pages/components/FaucetSender.tsx
@@ -1,6 +1,12 @@
-import { Coin, StargateClient } from "@cosmjs/stargate"
+import { Coin, StargateClient, SigningStargateClient } from "@cosmjs/stargate"
 import { ChangeEvent, Component, MouseEvent } from "react"
 import styles from '../../styles/Home.module.css'
+import { ChainInfo, Window as KeplrWindow } from "@keplr-wallet/types"
+import { AccountData, OfflineSigner } from "@cosmjs/proto-signing"
+
+declare global {
+    interface Window extends KeplrWindow {}
+}
 
 interface FaucetSenderState {
     denom: string
@@ -46,9 +52,119 @@ export class FaucetSender extends Component<FaucetSenderProps, FaucetSenderState
         toSend: e.currentTarget.value
     })
 
+    getTestnetChainInfo = (): ChainInfo => ({
+      chainId: "theta-testnet-001",
+      chainName: "theta-testnet-001",
+      rpc: "https://rpc.sentry-01.theta-testnet.polypore.xyz/",
+      rest: "https://rest.sentry-01.theta-testnet.polypore.xyz/",
+      bip44: {
+          coinType: 118,
+      },
+      bech32Config: {
+          bech32PrefixAccAddr: "cosmos",
+          bech32PrefixAccPub: "cosmos" + "pub",
+          bech32PrefixValAddr: "cosmos" + "valoper",
+          bech32PrefixValPub: "cosmos" + "valoperpub",
+          bech32PrefixConsAddr: "cosmos" + "valcons",
+          bech32PrefixConsPub: "cosmos" + "valconspub",
+      },
+      currencies: [
+          {
+              coinDenom: "ATOM",
+              coinMinimalDenom: "uatom",
+              coinDecimals: 6,
+              coinGeckoId: "cosmos",
+          },
+          {
+              coinDenom: "THETA",
+              coinMinimalDenom: "theta",
+              coinDecimals: 0,
+          },
+          {
+              coinDenom: "LAMBDA",
+              coinMinimalDenom: "lambda",
+              coinDecimals: 0,
+          },
+          {
+              coinDenom: "RHO",
+              coinMinimalDenom: "rho",
+              coinDecimals: 0,
+          },
+          {
+              coinDenom: "EPSILON",
+              coinMinimalDenom: "epsilon",
+              coinDecimals: 0,
+          },
+      ],
+      feeCurrencies: [
+          {
+              coinDenom: "ATOM",
+              coinMinimalDenom: "uatom",
+              coinDecimals: 6,
+              coinGeckoId: "cosmos",
+          },
+      ],
+      stakeCurrency: {
+          coinDenom: "ATOM",
+          coinMinimalDenom: "uatom",
+          coinDecimals: 6,
+          coinGeckoId: "cosmos",
+      },
+      coinType: 118,
+      gasPriceStep: {
+          low: 1,
+          average: 1,
+          high: 1,
+      },
+      features: ["stargate", "ibc-transfer", "no-legacy-stdTx"],
+  })
+
     // When the user clicks the "send to faucet button"
     onSendClicked = async(e: MouseEvent<HTMLButtonElement>) => {
-        alert("TODO")
+      const { keplr } = window
+      if (!keplr) {
+          alert("You need to install Keplr")
+          return
+      }
+      // Get the current state and amount of tokens that we want to transfer
+      const { denom, toSend } = this.state
+      const { faucetAddress, rpcUrl } = this.props
+      // Suggest the testnet chain to Keplr
+      await keplr.experimentalSuggestChain(this.getTestnetChainInfo())
+      // Create the signing client
+      const offlineSigner: OfflineSigner = window.getOfflineSigner!("theta-testnet-001")
+      const signingClient = await SigningStargateClient.connectWithSigner(
+          rpcUrl,
+          offlineSigner,
+      )
+      // Get the address and balance of your user
+      const account: AccountData = (await offlineSigner.getAccounts())[0]
+      this.setState({
+          myAddress: account.address,
+          myBalance: (await signingClient.getBalance(account.address, denom)).amount,
+      })
+      // Submit the transaction to send tokens to the faucet
+      const sendResult = await signingClient.sendTokens(
+          account.address,
+          faucetAddress,
+          [
+              {
+                  denom: denom,
+                  amount: toSend,
+              },
+          ],
+          {
+              amount: [{ denom: "uatom", amount: "500" }],
+              gas: "200000",
+          },
+      )
+      // Print the result to the console
+      console.log(sendResult)
+      // Update the balance in the user interface
+      this.setState({
+          myBalance: (await signingClient.getBalance(account.address, denom)).amount,
+          faucetBalance: (await signingClient.getBalance(faucetAddress, denom)).amount,
+      })
     }
 
     // The render function that draws the component at init and at state change
